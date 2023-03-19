8e39e089c7f073e370e355322fa8ed26aaab56e6
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-03-19 01:20:19 -0500

    Header

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Header from '../components/Header';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import Layout from '../containers/Layout';
@@ -10,6 +11,7 @@ import '../styles/global.scss';
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />

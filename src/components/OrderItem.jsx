f692cedfaf6e19562ef8a5849c26749b7a05fdc8
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-03-25 23:08:47 -0500

    delete product

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import '../styles/OrderItem.scss';
 import icon_close from "@icons/icon_close.png"
+import AppContext from "@context/AppContext"
+
 const OrderItem = ({product}) => {
 	const { title, price, images } = product;
+	const { removeFromCart } = useContext(AppContext);
+
+
+	const handleClick = (product) => {
+		console.log("remove product")
+		removeFromCart(product);
+	}
+
 	return (
 		<div className="OrderItem">
 			<figure>
@@ -10,7 +20,7 @@ const OrderItem = ({product}) => {
 			</figure>
 			<p>{title}</p>
 			<p>$ {price}</p>
-			<img src={icon_close} alt="close" />
+			<img src={icon_close} alt="close" onClick={()=> handleClick(product)} />
 		</div>
 	);
 }
--- src/hooks/UseInitialState.js
+++ src/hooks/UseInitialState.js
@@ -12,7 +12,13 @@ const UseInitialState = () => {
       cart: [...state.cart,payload]
     })
   }
-  return {state, addToCart};
+  const removeFromCart = (payload) => {
+    setState({
+      ...state,
+      cart: state.cart.filter(item => item.id !== payload.id)
+    })
+  }
+  return {state, addToCart, removeFromCart};
 }
 
 export default UseInitialState;

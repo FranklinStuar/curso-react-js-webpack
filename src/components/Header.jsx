226a7d9b315fa46cf3cea7941cf4012a454e6418
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2023-03-25 22:51:27 -0500

    show elements on cart/my order

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, {useState,useContext} from 'react';
 import Menu from '@components/Menu';
 import AppContext from '@context/AppContext';
+import MyOrder from '@containers/MyOrder';
 import '@styles/Header.scss';
 
 import menu from '@icons/icon_menu.svg';
@@ -10,9 +11,12 @@ import shoppingCart from '@icons/icon_shopping_cart.svg';
 const Header = () => {
 	const {state} = useContext(AppContext) 
 	const [toggle, setToggle] = useState(false);
+	const [toggleOrders, setToggleOrders] = useState(false);
+
 	const handleToggle = () => {
 		setToggle(!toggle);
 	}
+	
 	return (
 		<nav>
 			<img src={menu} alt="menu" className="menu" />
@@ -44,13 +48,14 @@ const Header = () => {
 					<li className="navbar-email" onClick={handleToggle}>
 						platzi@example.com
 					</li>
-					<li className="navbar-shopping-cart">
+					<li className="navbar-shopping-cart" onClick={()=>setToggleOrders(!toggleOrders)}>
 						<img src={shoppingCart} alt="shopping cart" />
 						<div>{state.cart.length}</div>
 					</li>
 				</ul>
 			</div>
 			{toggle && <Menu />}
+			{toggleOrders && <MyOrder />}
 
 		</nav>
 	);
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import OrderItem from '../components/OrderItem';
-import '../styles/MyOrder.scss';
+import React,{useContext} from 'react';
+import AppContext from '@context/AppContext';
+
+import OrderItem from '@components/OrderItem';
+import '@styles/MyOrder.scss';
+import flechita from "@icons/flechita.svg"
 
 const MyOrder = () => {
+	const {state} = useContext(AppContext) 
+	const totalCart = state.cart.reduce(
+		(accumulator, currentValue) => accumulator + currentValue.price, 0
+	);
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
-				<img src="./icons/flechita.svg" alt="arrow" />
+				<img src={flechita} alt="arrow" />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map((product, index) => (
+					<OrderItem product={product} key={index}/>
+				))}
 				<div className="order">
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${totalCart}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
